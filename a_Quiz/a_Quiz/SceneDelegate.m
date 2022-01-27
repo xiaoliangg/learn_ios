a2697320a5019bea8c128134a4b8240aa068fcbc
yl <yl@yldeMacBook-Pro.local>
2022-01-27 21:25:32 +0800

    fix: start app success

--- a_Quiz/a_Quiz/SceneDelegate.m
+++ a_Quiz/a_Quiz/SceneDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "SceneDelegate.h"
+#import "BNRQuizViewController.h"
 
 @interface SceneDelegate ()
 
@@ -18,6 +19,9 @@
     // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        BNRQuizViewController *quizVC = [[BNRQuizViewController alloc] init];
+        self.window.rootViewController = quizVC;
 }
 
 
