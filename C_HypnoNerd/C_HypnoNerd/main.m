a84e024358112164b60d0bb542f75143f30a28f6
yl <yl@yldeMacBook-Pro.local>
2022-02-01 18:46:40 +0800

    C_HypnoNerd UIApplicationMain()和main() 的作用

--- C_HypnoNerd/C_HypnoNerd/main.m
+++ C_HypnoNerd/C_HypnoNerd/main.m
@@ -8,6 +8,12 @@
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
+/**
+ UIApplicationMain函数作用:
+ 1、创建 UIApplication 对象，该对象用来维护运行循环
+ 2、创建并设置 UIApplication 对象的 delegate
+ 3、启动运行循环并开始接收事件前，UIApplication 对象会向其delegate发送 application:didFinishLaunchingWithOptions 的消息，使应用能有机会完成相应的初始化工作
+ */
 int main(int argc, char * argv[]) {
     NSString * appDelegateClassName;
     @autoreleasepool {
