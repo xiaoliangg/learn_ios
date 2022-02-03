87c282fa5358a41a109c5768065fe9f07e529d08
yl <yl@yldeMacBook-Pro.local>
2022-02-03 15:43:02 +0800

    E_TouchTracker 12.2 初始化工程

---
+++ E_TouchTracker/E_TouchTracker/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  E_TouchTracker
+//
+//  Created by yl on 2022/2/3.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ E_TouchTracker/E_TouchTracker/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  E_TouchTracker
+//
+//  Created by yl on 2022/2/3.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
