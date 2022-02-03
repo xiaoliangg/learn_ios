666268cc6f632b355abdbf8e06bff621fd138b77
yl <yl@yldeMacBook-Pro.local>
2022-02-03 11:04:55 +0800

    D_Homepwoner 10.2创建BNRDetailViewController

---
+++ D_Homepwner/D_Homepwner/BNRDetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  BNRDetailViewController.h
+//  D_Homepwner
+//
+//  Created by yl on 2022/2/3.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BNRDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ D_Homepwner/D_Homepwner/BNRDetailViewController.m
@@ -0,0 +1,21 @@
+//
+//  BNRDetailViewController.m
+//  D_Homepwner
+//
+//  Created by yl on 2022/2/3.
+//
+
+#import "BNRDetailViewController.h"
+
+@interface BNRDetailViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *serialNumberField;
+@property (weak, nonatomic) IBOutlet UITextField *valueField;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+
+
+@end
+
+@implementation BNRDetailViewController
+
+@end
