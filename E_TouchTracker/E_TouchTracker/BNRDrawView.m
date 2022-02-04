6188951f95f65e05d2e2c4f0ee24cfea1ef136d2
yl <yl@yldeMacBook-Pro.local>
2022-02-04 14:08:24 +0800

    E_TouchTracker 13.2 13.3 识别双击、单击、单击改变颜色

--- E_TouchTracker/E_TouchTracker/BNRDrawView.m
+++ E_TouchTracker/E_TouchTracker/BNRDrawView.m
@@ -12,7 +12,8 @@
 
 @property (nonatomic,strong) NSMutableDictionary *linesInProgress;
 @property (nonatomic,strong) NSMutableArray *finishedLines;
-
+// 单击选择的线条；弱引用
+@property (nonatomic,weak) BNRLine *selectedLine;
 @end
 @implementation BNRDrawView
 
@@ -25,6 +26,23 @@
         self.finishedLines = [[NSMutableArray alloc] init];
         self.backgroundColor = [UIColor grayColor];
         self.multipleTouchEnabled = YES;
+        
+        // 添加双击手势
+        UITapGestureRecognizer *doubleTapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(doubleTap:)];
+        doubleTapRecognizer.numberOfTapsRequired = 2;
+        // 避免双击的第一下点击出现小圆点儿
+        doubleTapRecognizer.delaysTouchesBegan = YES;
+        [self addGestureRecognizer:doubleTapRecognizer];
+        
+        // 添加单击手势
+        UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tap:)];
+        tapRecognizer.delaysTouchesBegan = YES;
+        // 确定不是双击，才识别单击
+        // 避免双击时，触发单击手势
+        [tapRecognizer requireGestureRecognizerToFail:doubleTapRecognizer];
+        [self addGestureRecognizer:tapRecognizer];
+        
+        //
     }
     return self;
 }
@@ -53,6 +71,12 @@
     [[UIColor redColor] set];
     for(NSValue *key in self.linesInProgress){
         [self strokeLine:self.linesInProgress[key]];
+    }
+    
+    // 用绿色绘制选中线条
+    if(self.selectedLine){
+        [[UIColor greenColor] set];
+        [self strokeLine:self.selectedLine];
     }
 }
 
@@ -108,4 +132,50 @@
     }
     [self setNeedsDisplay];
 }
+
+#pragma mark - 手势识别
+
+- (void)doubleTap:(UIGestureRecognizer *)gr
+{
+    NSLog(@"Recognized Double Tap");
+    
+    [self.linesInProgress removeAllObjects];
+    [self.finishedLines removeAllObjects];
+    [self setNeedsDisplay];
+}
+
+- (void)tap:(UIGestureRecognizer *)gr
+{
+    NSLog(@"Recognized Tap");
+    
+    CGPoint point = [gr locationInView:self];
+    self.selectedLine = [self lineAtPoint:point];
+    
+    [self setNeedsDisplay];
+}
+
+/**
+ 找到离p点最近点线条
+ */
+- (BNRLine *)lineAtPoint:(CGPoint)p
+{
+    // 找出离p最近的BNRLine对象
+    for (BNRLine *l in self.finishedLines) {
+        CGPoint start = l.begin;
+        CGPoint end = l.end;
+        
+        // 检查线条的若干点
+        for (float t = 0.0; t<=1.0; t += 0.5) {
+            float x = start.x + t*(end.x - start.x);
+            float y = start.y + t*(end.y - start.y);
+            // 如果线条的某个点和p的距离中20点以内，就返回响应的BNRLine对象
+            if(hypot(x-p.x, y-p.y) < 20.0){
+                return l;
+            }
+        }
+    }
+    // 如果没能找到符合条件的线条，就返回nil，代表不选择任何线条
+    return nil;
+}
 @end
+
