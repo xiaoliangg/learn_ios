9e760b2278ea889155c05a1a9cb4e6984da5b7e7
xiaoliangg <xiaoliangyuu@163.com>
2022-02-21 23:51:17 +0800

    G_Colorboard 28.3 Segue关联多个UIViewController

---
+++ G_Colorboard/G_Colorboard/BNRColorViewController.h
@@ -0,0 +1,16 @@
+//
+//  BNRColorViewController.h
+//  G_Colorboard
+//
+//  Created by yl on 2022/2/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BNRColorViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ G_Colorboard/G_Colorboard/BNRColorViewController.m
@@ -0,0 +1,16 @@
+//
+//  BNRColorViewController.m
+//  G_Colorboard
+//
+//  Created by yl on 2022/2/21.
+//
+
+#import "BNRColorViewController.h"
+
+@implementation BNRColorViewController
+
+- (IBAction)dismiss:(id)sender
+{
+    [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
+}
+@end
